9e1c43db56c5f798f0014b30ba5eaf9a578667d5
ivillanuu <ivillanu@stevens.edu>
2024-06-26 19:43:10 -0400

    started level map

---
+++ scenes/level/level_1/level_1.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@onready var light_detection := $DirectionalLight3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GameData.current_light = light_detection
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
