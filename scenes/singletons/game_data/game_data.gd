ff2263e177591b468ee61e0be0872c45cfd3db73
migski-dev <155409914+migski-dev@users.noreply.github.com>
2024-06-26 19:41:40 -0400

    Feature/moving shadow platform (#9)
    players move along moving shadows
    
    known bugs: player can sometimes break out of a moving shadow

--- scenes/singletons/game_data/game_data.gd
+++ scenes/singletons/game_data/game_data.gd
@@ -2,6 +2,12 @@ extends Node
 
 var current_light:  Light3D
 var light_blocking_object
+var local_collision_position: Vector3
+
+func is_light_blocking_object_moving() -> bool:
+	if(light_blocking_object == null):
+		return false
+	return light_blocking_object.is_in_group('moving_objects')
 
 func on_player_interact():
 	if light_blocking_object.is_in_group("interactable_group") and light_blocking_object.has_method("_on_interacted"):
